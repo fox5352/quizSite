6dce41767d152312c422cb00ca951f12d9897399
Chris <voschristopher53@gmail.com>
2022-11-08 17:52:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+    "use strict";
+    const el = document.querySelectorAll('.circle');
+    const questionPage = document.querySelector('.Question');
+    const checkbox = document.querySelectorAll('.inputbox')
+    const labels = document.querySelectorAll('label')
+
+    let pageNum = 0;
+    let comparePageNum = 1;
+    let randNum = 0;
+
+    let score = 0;
+
+    let data = new Object;
+    let question = new Object;
+    let response = new Object
+
+
+    async function request() {
+        data = await fetch('https://github.com/fox5352/quizWebPage/blob/data-storage/listOfQuestions.json').then(response => response.json());
+        pageload();
+    }
+
+    function getAnswer() {
+        if (pageNum < 12) {
+            let checkbox = $('.inputbox')
+            let answer = data["qestion" + String(comparePageNum)][0]['answer']
+            for (let i = 0; i < checkbox.length; i++) {
+                if (checkbox[i].checked) {
+                    console.log()
+                    if (checkbox[i].name == answer) {
+                        console.log(checkbox[i].name, answer);
+                        randNum = 1
+                    }
+                    else {
+                        console.log(checkbox[i].name, answer);
+                        randNum = 0
+                    }
+                    console.log(randNum);
+                }
+            }
+
+        }
+    }
+
+    function removeActiveClasses() {
+        if (comparePageNum === 12) {
+            questionPage.innerHTML = `congrats on completing the quiz <br> <span>your score is : ${score}</span>`
+        }
+        if (pageNum != el.length) {
+            el[pageNum].classList.remove('active');
+
+            if (randNum === 1) {
+                el[pageNum].classList.add("true");
+                score++
+            }
+            else {
+                el[pageNum].classList.add("true");
+                el[pageNum].classList.add('false');
+            }
+            pageNum++;
+            comparePageNum++;
+        }
+        if (comparePageNum === el.length) {
+            document.querySelector('#next').innerHTML = "End";
+        }
+        if (pageNum != el.length) {
+            el[pageNum].classList.add('active');
+        }
+    }
+
+    function pageload() {
+        // questionPage.innerHTML 
+        if (pageNum < 12) {
+            questionPage.innerHTML = data["qestion" + String(comparePageNum)][0]["qestion"]
+            labels.forEach(label => {
+                label.innerText = data["qestion" + String(comparePageNum)][0][label.id]
+            })
+        }
+    }
+
+    function update() {
+        const actives = document.querySelectorAll('.true');
+        const progress = document.getElementById('progress');
+        progress.style.width = (actives.length - 1) / (el.length - 1) * 100 + "%";
+        for (let i = 0; i < checkbox.length; i++) {
+            checkbox[i].checked = false
+        }
+        pageload();
+    }
+
+    request();
+
+    const btn = document.querySelector('#next');
+    btn.addEventListener("click", () => {
+        getAnswer();
+        removeActiveClasses();
+        update();
+    })
+
+
+})
